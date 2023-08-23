12edbe5afcd894b2ce0b16614883d35e8b90ccac
Patrick Coxall <mr.coxall@mths.ca>
2023-08-22 21:22:25 -0400

    update code

--- main.ts
+++ main.ts
@@ -2,7 +2,7 @@
  *
  * Created by: Mr. Coxall
  * Created on: Sep 2020
- * This program ...
+ * This program shows "Hello, MTHS!"
 */
 
-basic.showString('Hello, World!')
+basic.showString('Hello, MTHS!')
